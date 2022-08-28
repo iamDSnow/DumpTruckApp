2100ab2212be51817e9c9f2f74b01828148d2fd1
Derrick Snow <74995064+suprawhiz@users.noreply.github.com>
2022-08-27 23:48:45 -0700

    master

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,26 +1,26 @@
-const mail = require('@sendgrid/mail')
+// const mail = require('@sendgrid/mail')
 
-export default async function handler (req, res) {
-  const appContractor = req.contractor
+// export default async function handler (req, res) {
+//   const appContractor = req.contractor
 
-  const msg = {
-    to: 'suprawhiz@gmail.com',
-    from: 'suprawhiz@gmail.com',
-    subject: 'JOB APPLICATION:',
-    text: appContractor,
-    html: appContractor
-  }
+//   const msg = {
+//     to: 'suprawhiz@gmail.com',
+//     from: 'suprawhiz@gmail.com',
+//     subject: 'JOB APPLICATION:',
+//     text: appContractor,
+//     html: appContractor
+//   }
 
-  await mail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID)
+//   await mail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID)
 
-  await mail.send(msg)
+//   await mail.send(msg)
 
-  await (res => {
-    return res.status(200).json({ status: 'Ok' })
-  })
+//   await (res => {
+//     return res.status(200).json({ status: 'Ok' })
+//   })
 
-  await (error => {
-    console.error('error')
-    return error
-  })
-}
+//   await (error => {
+//     console.error('error')
+//     return error
+//   })
+// }
