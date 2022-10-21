aa1346861c0af67dc3732abd398ceced5169bc65
Derrick Snow <74995064+suprawhiz@users.noreply.github.com>
2022-10-21 12:06:05 -0700

    Update mail.js

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,32 +1,42 @@
 
+
 export default async function  handler(req , res) {
+  // const mail = require('@sendgrid/mail')
 
-  const sgMail = require('@sendgrid/mail')
-  // const SENDGRID_API = 'https://api.sendgrid.com/v3/mail/send'
-
-sgMail.setApiKey('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')
-
+  const SENDGRID_API = 'https://api.sendgrid.com/v3/mail/send'
+  
   const data = JSON.stringify(req.body)
 
   console.log(data.notesInput)
 
 
-  const msg = {
-    to: 'suprawhiz@gmail.com', // Change to your recipient
-    from: 'snow.derrickl@gmail.com', // Change to your verified sender
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  const msg = await {
+    to: 'suprawhiz@gmail.com',
+    from: 'snow.derrickl@gmail.com',
+    subject: 'JOB APPLICATION:',
+    text: 'test',
+    html: 'test'
+  }
+  try{
+  await fetch(
+    SENDGRID_API,
+    {
+      method: "POST",
+      body: msg,
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_SENDGRID_API_KEY}`,
+        }
+  )
+ 
+
+  return res.status(200).json({ revalidated: true })
+
+}
+  catch (err){
+    return res.status(500).json({message: 'something went wrong'})
   }
 
-  sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })}
+}
+
 // export { sendEmail };
 
 // export default async function handler (req, res) {
